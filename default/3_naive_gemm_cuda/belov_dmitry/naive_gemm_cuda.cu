26a87e9dc3a4592e83679e6135486da65c12e560
d.belov <d.belov@yadro.com>
2026-07-04 00:49:34 +0300

    3_naive_gemm_cuda task

---
+++ default/3_naive_gemm_cuda/belov_dmitry/naive_gemm_cuda.cu
@@ -0,0 +1,53 @@
+#include "naive_gemm_cuda.h"
+
+#include <cuda_runtime.h>
+
+__global__ void naiveGennFunc(const float* mtxA, const float* mtxB, float* mtxC, int mtxSize)
+{
+    int colIndex = blockIdx.x * blockDim.x + threadIdx.x;
+    int rowIndex = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (colIndex < mtxSize && rowIndex < mtxSize)
+    {
+        int tmpIndex = rowIndex * mtxSize;
+        float resElC = 0.0f;
+        for (int i = 0; i < mtxSize; ++i)
+            resElC += mtxA[tmpIndex + i] * mtxB[i * mtxSize + colIndex];
+
+        mtxC[tmpIndex + colIndex] = resElC;
+    }
+}
+
+std::vector<float> NaiveGemmCUDA(const std::vector<float>& a,
+                                 const std::vector<float>& b,
+                                 int n) 
+{
+    const int sizeMtx = static_cast<int>(a.size());
+    const size_t bitSizeMtx = sizeMtx * sizeof(float);
+
+    float *deviceMtxA = nullptr;
+    cudaMalloc(&deviceMtxA, bitSizeMtx);
+    cudaMemcpy(deviceMtxA, a.data(), bitSizeMtx, cudaMemcpyHostToDevice);
+
+    float *deviceMtxB = nullptr;
+    cudaMalloc(&deviceMtxB, bitSizeMtx);
+    cudaMemcpy(deviceMtxB, b.data(), bitSizeMtx, cudaMemcpyHostToDevice);
+
+    float *deviceMtxC = nullptr;
+    cudaMalloc(&deviceMtxC, bitSizeMtx);
+
+    const size_t numThreads = 16;
+    dim3 threadsPerBlock(numThreads, numThreads);
+    const size_t numBlocks = (sizeMtx + numThreads - 1) / numThreads;
+    dim3 blockCount(numBlocks, numBlocks);
+    naiveGennFunc<<<blockCount, threadsPerBlock>>>(deviceMtxA, deviceMtxB, deviceMtxC, n);
+
+    std::vector<float> output(sizeMtx);
+    cudaMemcpy(output.data(), deviceMtxC, bitSizeMtx, cudaMemcpyDeviceToHost);
+
+    cudaFree(deviceMtxC);
+    cudaFree(deviceMtxB);
+    cudaFree(deviceMtxA);
+
+    return output;
+}
